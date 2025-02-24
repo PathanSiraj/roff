9d6f2f569cfc23fb838ed5ee0a4a529550a3faaa
Pathan Siraj <pathansiraj11092006@gmail.com>
2025-02-24 16:41:59 +0530

    Add files via upload

---
+++ 4-3.py
@@ -0,0 +1,6 @@
+import random
+friends=['a','b','c','d','e']
+print(random.choice(friends))#choice is used for randomness
+rand=random.randint(0,4)
+print(friends[rand])#it gives the no.place item in freinds
+print(rand)#it gives numbers
