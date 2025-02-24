9ce8af7769edaf98e89126f054d01e810f2c918a
Pathan Siraj <pathansiraj11092006@gmail.com>
2025-02-24 16:25:22 +0530

    Add files via upload

---
+++ 4-2.py
@@ -0,0 +1,10 @@
+usa=['a','b','c','d','e']
+usa.append('f')
+#append takes only one argument
+#you tery to add in suare brackets by use it print square bracket also
+usa.extend(['g','h','i'])
+#you can add many arguments in square brackets
+print(usa)
+print(usa[0])#0 number indicates the place
+usa[1]='b'
+print(usa[1])
